30f32789e7fe2433fc579dfe04f0b94f118775bd
John Muiruri <jontedev@gmail.com>
2021-07-03 14:00:30 +0300

    replaced goods with commodities, both goods and services to be in commodities

---
+++ database/migrations/2021_07_03_103445_create_commodities_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommoditiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('commodities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('description', 255);
+            //foreign keys
+            $table->foreignId('seller_id')->constrained('users');
+            $table->foreignId('category_id')->constrained('categories');
+            
+            $table->bigInteger('price');
+            $table->bigInteger('minimum_amount');
+            $table->enum('type', ['goods', 'services']);
+            $table->json('exchange_for', 255);// key=good/service; value=goods/service id
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('commodities');
+    }
+}
---
+++ database/migrations/2021_07_03_105052_create_photos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('path', 255);
+            //foreign keys
+            $table->foreignId('commodity_id')->constrained('commodities');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photos');
+    }
+}
